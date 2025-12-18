3efb537c67cd78f82a4d1821939eb7693965a304
SmittyB00p <tsmith.smith391@gmail.com>
2025-12-18 12:16:48 -0600

    utils.py commit

--- Notebooks/models/utils.py
+++ Notebooks/models/utils.py
@@ -6,12 +6,19 @@ def return_scores(estimator, X_train, X_test, y_train, y_test):
     train_score = estimator.score(X_train, y_train)
     test_score = estimator.score(X_test, y_test)
 
+    train_pred = estimator.predict(X_train)
+    test_pred = estimator.predict(X_test)
+
     train_recall = recall_score(y_train, estimator.predict(X_train))
     test_recall = recall_score(y_test, estimator.predict(X_test))
 
+    train_prec = precision_score(y_train, train_pred)
+    test_prec = precision_score(y_test, test_pred)
+
     df = pd.DataFrame({
-        'Train Scores': [train_score, train_recall],
-        'Test Scores': [test_score, test_recall]
-    }, index=['Accuracy', 'Recall'])
+        'Accuracy': [train_score, test_score],
+        'Recall': [train_recall, test_recall],
+        'Precision': [train_prec, test_prec]
+    }, index=['Train', 'Test'])
 
     return df
